8625d84be2ee73f2e1bc61a63b6a866bec1311c9
amcneel <amcneel@wustl.edu>
2016-11-13 20:32:29 -0600

    w/o creative portion

--- chatserver.js
+++ chatserver.js
@@ -27,33 +27,24 @@ io.sockets.on("connection", function(socket){
 		socket.broadcast.to('default').emit('updateChat', 'SERVER', username+" has connected");
 		socket.emit('updateRooms', rooms, 'default');
 		io.sockets.emit('updateUsers', users, socket.username);
-		console.log(users);
+		io.sockets.emit('allPasswords', passwords);
 
 	});
 	socket.on('switchRoom', function(newRoom, user){
 		
-		var canSwitch = true;
-		for(var i=0; i<bans.length; i++){
-			console.log(Object.keys(bans[i]));
-			console.log(bans[i]);
-			if(newRoom == Object.keys(bans[i]) && user == bans[i].newRoom){
-				canSwitch = false;
-			}
-		}
-		if(canSwitch == true){
 		//leave current room in session
-		socket.username = user;
-		users[user] = newRoom;
-		socket.leave(socket.room);
-		socket.join(newRoom);
-		socket.emit('updateChat', 'SERVER', 'connected to '+newRoom);
-		socket.broadcast.to(socket.room).emit('updateChat', 'SERVER', socket.username+' has left');
-		io.sockets.in(newRoom).emit('updateChat', 'SERVER', socket.username + ' has joined this room');
-		socket.room = newRoom;
-		socket.emit('updateRooms', rooms, newRoom);	
-		io.sockets.emit('updateUsers', users, socket.username);
-		socket.emit('clearChat', newRoom);
-		}
+			socket.username = user;
+			users[user] = newRoom;
+			socket.leave(socket.room);
+			socket.join(newRoom);
+			socket.emit('updateChat', 'SERVER', 'connected to '+newRoom);
+			socket.broadcast.to(socket.room).emit('updateChat', 'SERVER', socket.username+' has left');
+			io.sockets.in(newRoom).emit('updateChat', 'SERVER', socket.username + ' has joined this room');
+			socket.room = newRoom;
+			socket.emit('updateRooms', rooms, newRoom);	
+			io.sockets.emit('updateUsers', users, socket.username);
+			socket.emit('clearChat', newRoom);
+		
 	});
 
 	socket.on('create_room', function(newroom, creator, password){
@@ -66,22 +57,20 @@ io.sockets.on("connection", function(socket){
 	});
 
 	socket.on('message_to_server', function(data) {
-		console.log("message: "+data["message"]);
-		console.log(users[socket.username]);
+		//console.log("message: "+data["message"]);
+		//console.log(users[socket.username]);
 		socket.room = users[socket.username];
 		io.sockets.in(socket.room).emit("updateChat", socket.username, data['message']);
 	});
 
 	socket.on('direct_message', function(msg, target_user, user){
-		console.log("dm: "+msg);
+		// console.log("dm: "+msg);
 		io.sockets.emit('dm_client', target_user, user, msg);
 	});
 
 	socket.on('kick_setup', function(target_user, given_room){
-		console.log(target_user+" kicked");
-		if(given_room == 'default'){
-
-		}
+		// console.log(target_user+" kicked");
+		if(given_room == 'default'){}
 		else if(given_room == users[target_user]){
 			io.sockets.emit('kicking', target_user);
 		}	
@@ -96,36 +85,30 @@ io.sockets.on("connection", function(socket){
 		socket.broadcast.to('default').emit('updateChat', 'SERVER', user+' has joined this room');
 		socket.emit('updateRooms', rooms, users[user]);
 		io.sockets.emit('updateUsers', users, socket.username);
-		console.log(users);
 		socket.emit('clearChat', 'default');
 	});
 
 	socket.on('ban_setup', function(target_user, given_room){
-		if(given_room == 'default'){
-
-		}
-		else if(given_room == users[target_user]){
+		console.log('given room: '+given_room);
+		console.log('room target is in: '+users[target_user]);
+		if(given_room == 'default'){}
+		else if(users[target_user] == given_room){
 			io.sockets.emit('banning', target_user, given_room);
 		}
+		else{}
 	});
 
-	socket.on('ban', function(user, given_room){
-		console.log(bans);
-		var key = given_room;
-		var obj = {};
-		obj[key] = user;
-		bans.push(obj);	
-		console.log(bans);
-		io.sockets.emit('updateBans', bans);
-		socket.broadcast.to(users[user]).emit('updateChat', 'SERVER', user+' has been banned from this room');
-		socket.leave(socket.room);
-		users[user] = 'default';
-		socket.join(users[user]);
+	socket.on('complete_ban', function(given_user){
+		socket.broadcast.to(users[given_user]).emit('updateChat', 'SERVER', given_user+ ' has been BANNED');
+		socket.leave(users[given_user]);
+		users[given_user] = 'default';
+		socket.join('default');
+		socket.broadcast.to('default').emit('updateChat', 'SERVER', given_user+' has joined');
 		socket.emit('updateChat', 'SERVER', 'You have been banned from this room');
-		socket.broadcast.to('default').emit('updateChat', 'SERVER', user+' has joined');
-		socket.emit('updateRooms', rooms, users[user]);
-		io.sockets.emit('updateUsers', users, socket.username);
 
+		socket.emit('clearChat', 'default');
+		socket.emit('updateRooms', rooms, users[given_user]);
+		io.sockets.emit('updateUsers', users, given_user);
 	});
 
 });
